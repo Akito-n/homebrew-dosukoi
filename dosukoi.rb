1f876f4361225c10082a115a711a6820b0306287
Akito-n <naki415@outlook.jp>
2025-02-04 19:17:14 +0900

    Update SHA256 for new dosukoi-mac.zip

--- dosukoi.rb
+++ dosukoi.rb
@@ -2,8 +2,8 @@ class Dosukoi < Formula
   desc "A simple Docker container stopping tool"
   homepage "https://github.com/Akito-n/dosukoi"
   url "https://github.com/Akito-n/dosukoi/releases/download/v0.1.0/dosukoi-mac.zip"
-  sha256 "7e8db5dc240600750051e85f651d99eb7a6e8a06f4265807dded4f59ac430582"
-  version "0.1.0"
+  sha256 "49eca9d3ab9e74737b8d7a993a55d5d51779981d4c91bad69dcfaaac8e69cf69"
+  version "0.1.1"
 
   def install
     bin.install "dosukoi"
