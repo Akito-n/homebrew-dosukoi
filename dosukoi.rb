93d91ce137de9b66e7a500bd967fde257a9a6c03
Akito-n <naki415@outlook.jp>
2025-02-07 17:12:27 +0900

    add hash

--- dosukoi.rb
+++ dosukoi.rb
@@ -2,7 +2,7 @@ class Dosukoi < Formula
   desc "A simple Docker container stopping tool"
   homepage "https://github.com/Akito-n/dosukoi"
   url "https://github.com/Akito-n/dosukoi/releases/download/v0.1.4/dosukoi-mac.zip"
-  sha256 "00a715641738658d399ab252452595de1d34e78d5e464256b3ed836f259b652c"
+  sha256 "ac0ff2a6c2006657e78ca467debd931120b0ab237d28701e7151b70b07adf2c0"
   version "0.1.4"
 
   def install
