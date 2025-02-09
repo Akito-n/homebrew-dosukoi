a9dcaab8f3f141ec0cc16f9b2b3dad208694ddd3
Akito-n <naki415@outlook.jp>
2025-02-09 15:07:20 +0900

    v0.1.5

--- dosukoi.rb
+++ dosukoi.rb
@@ -1,9 +1,9 @@
 class Dosukoi < Formula
   desc "A simple Docker container stopping tool"
   homepage "https://github.com/Akito-n/dosukoi"
-  url "https://github.com/Akito-n/dosukoi/releases/download/v0.1.4/dosukoi-mac.zip"
-  sha256 "ac0ff2a6c2006657e78ca467debd931120b0ab237d28701e7151b70b07adf2c0"
-  version "0.1.4"
+  url "https://github.com/Akito-n/dosukoi/releases/download/v0.1.5/dosukoi-mac.zip"
+  sha256 "d4b84c3e1dd351499ebbf0412175c9282ea8251dfab499b70f41ed29a293e56c"
+  version "0.1.5"
 
   def install
     bin.install "dosukoi"
